e5f63868ded70cbcb6789b4a022d3e9b077f28fd
Wesley69KX <84612535+Wesley69KX@users.noreply.github.com>
2025-12-04 01:08:48 -0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gestao-torres-v15'; // Mudamos para v3 para forçar atualização
+const CACHE_NAME = 'gestao-torres-v16'; // Mudamos para v3 para forçar atualização
 const ASSETS = [
   '/',
   '/index.html',
