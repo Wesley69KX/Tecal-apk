1975d180f8475147083c1e2997b130738890ec15
Wesley69KX <84612535+Wesley69KX@users.noreply.github.com>
2025-12-09 19:19:33 -0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gestao-torres-v21'; // Mudei para v8 para forçar atualização
+const CACHE_NAME = 'gestao-torres-v22'; // Mudei para v8 para forçar atualização
 const ASSETS = [
   '/',
   '/index.html',
